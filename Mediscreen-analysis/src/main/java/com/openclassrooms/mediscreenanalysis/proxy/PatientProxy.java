b1965d7def33eb494c199b2c9312a53bd06006fe
chubaka59 <joffrey.lefebvre@gmail.com>
2023-09-13 11:35:54 +0200

    updated feign configuration to get the url in the application.properties. created .env file to erase the properties if running in docker. added mysql and mongodb configuration in docker-compose.yaml.

--- Mediscreen-analysis/src/main/java/com/openclassrooms/mediscreenanalysis/proxy/PatientProxy.java
+++ Mediscreen-analysis/src/main/java/com/openclassrooms/mediscreenanalysis/proxy/PatientProxy.java
@@ -6,10 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// Feign config when running locally
-@FeignClient(name = "mediscreen-patient", url = "localhost:9001")
-//Feign config when running with docker
-//@FeignClient(name = "mediscreen-patient", url = "mediscreen-patient:9001")
+@FeignClient(name = "mediscreen-patient")
 public interface PatientProxy {
 
     @GetMapping(value = "/patients/{id}")
